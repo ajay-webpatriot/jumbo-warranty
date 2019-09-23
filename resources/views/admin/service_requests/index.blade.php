4a51797738fe50bf4ca4b7a5c299387ce2d8731a
ajay-webpatriot <rajdip.webpatriot@gmail.com>
2019-09-23 15:32:33 +0530

    add column in excel and remove status from filter

--- resources/views/admin/service_requests/index.blade.php
+++ resources/views/admin/service_requests/index.blade.php
@@ -554,7 +554,7 @@
                         title: 'Service request',
                         sheetName: 'Exported Service request',
                         exportOptions: {
-                            columns: [1, 2, 3, 4, 5, 6, 7]
+                            columns: [0, 1, 2, 3, 4, 5,6]
                         },
                         customize: function (xlsx) { 
                            
@@ -954,7 +954,7 @@
                         title: 'Service request',
                         sheetName: 'Exported Service request',
                         exportOptions: {
-                            columns: [1, 2, 3, 4, 5, 6, 7, 8, 9,10,11]
+                            columns: [0,1, 2, 3, 4, 5, 6, 7, 8, 9,10,11]
                         },
                         customize: function (xlsx) { 
                            
